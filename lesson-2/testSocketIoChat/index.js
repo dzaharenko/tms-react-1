1afa92f54566528f2108b77b6c4c700b93d65a84
Andrew <abogomolov90@gmail.com>
2020-03-11 19:03:53 +0300

    Updated testSocketIoChat

--- lesson-2/testSocketIoChat/index.js
+++ lesson-2/testSocketIoChat/index.js
@@ -3,11 +3,11 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('a user connected');
 
   socket.on('disconnect', function() {
@@ -15,9 +15,13 @@ io.on('connection', function(socket){
   });
 
   socket.on('chat message', function(msg) {
-    console.log('message: ' + msg);
+    console.log(`message from ${socket.name}: ${msg}`);
 
-    io.emit('chat message', msg);
+    io.emit('chat message', `${socket.name}: ${msg}`);
+  });
+
+  socket.on('setName', function(name) {
+    socket.name = name;
   });
 });
 
